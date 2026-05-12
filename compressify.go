626eb840a39ee1aaaf88ea5fc4ff52f920433d75
Miguel Alves <migueldealmeidaalves55@gmail.com>
2026-05-11 21:30:42 -0300

    parse 100 bytes and transforms into a string

--- compressify.go
+++ compressify.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,5 +14,35 @@ func main() {
 		return
 	}
 
-	fmt.Println("Hello, world!")
+	file, err := os.Open(args[1])
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	defer file.Close()
+
+	bytes := make([]byte, 100)
+
+	n, err := file.Read(bytes)
+	if err != nil {
+		fmt.Println("Error reading bytes:", err)
+		return
+	}
+	
+	c := 1
+	var sb strings.Builder
+
+	for i, byte := range bytes {
+		if i + 1 < len(bytes) && byte == bytes[i + 1] {
+			c++
+		} else {
+			sb.WriteByte(byte)
+		}
+	}
+
+	result := sb.String()
+	fmt.Println(result)
+
+	fmt.Printf("Read %d bytes: %v\n", n, bytes)
 }
